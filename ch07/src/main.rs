f47758413d6e23502eea7f482eec03d889aaaeda
sigpher <425223341@qq.com>
2023-11-09 17:15:53 +0800

    try thirtyfour,but got bugs

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use thirtyfour::{
+    prelude::{ElementQueryable, WebDriverError},
+    By, DesiredCapabilities, EdgeCapabilities, WebDriver,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), WebDriverError> {
+    // let caps = EdgeCapabilities::new();
+    let caps = DesiredCapabilities::chrome();
+
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    let url = "https://spa1.scrape.center/";
+    driver.goto(url).await?;
+    let check = driver.find(By::Css(".m-b-sm")).await?;
+    println!("{:?}", check);
+    driver.quit().await?;
+
+    Ok(())
 }
+
