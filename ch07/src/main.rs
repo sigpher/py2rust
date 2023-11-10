47254f60c04bf3557fe6525450a228cc4fcf6a94
sigpher <425223341@qq.com>
2023-11-10 23:36:08 +0800

    anti shield with chreome devtools

--- ch07/src/main.rs
+++ ch07/src/main.rs
@@ -1,17 +1,25 @@
-use thirtyfour::prelude::*;
+use std::{thread::sleep, time::Duration};
+
+use serde_json::json;
+use thirtyfour::{extensions::cdp::ChromeDevTools, prelude::*};
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
-    let caps = DesiredCapabilities::edge();
+    let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
-
-    // Navigate to https://wikipedia.org.
-    driver.goto("www.baidu.com").await?;
-
-    // Click the search button.
-    // Always explicitly close the browser.
+    //anti shield
+    /////////////////////////////
+    let dev_tools = ChromeDevTools::new(driver.handle.clone());
+    dev_tools
+        .execute_cdp_with_params(
+            "Page.addScriptToEvaluateOnNewDocument",
+            json!({"source": "Object.defineProperty(navigator, 'webdriver', {get:()=>undefined})" }),
+        )
+        .await?;
+    /////////////////////////////
+    driver.goto("https://ssr1.scrape.center/").await?;
+    sleep(Duration::from_secs(5));
     driver.quit().await?;
 
     Ok(())
 }
-
